ff3fe888a8733009fa342caba4ba4a53adb4ec0a
Jupalaja <jupalaja@gmail.com>
2024-05-18 13:31:49 -0500

    cleaned audioO

--- audio_waveform_app.py
+++ audio_waveform_app.py
@@ -7,37 +7,40 @@ import soundfile as sf
 import logging
 import warnings
 
-# Configure logging to reduce verbosity
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
-
-# Suppress specific Matplotlib warnings
 warnings.filterwarnings("ignore", category=UserWarning, module='matplotlib')
 
-# Title of the app
 st.title('Audio File Uploader and Waveform Plotter')
-
-# Upload audio file
 uploaded_file = st.file_uploader("Choose an audio file", type=["wav", "mp3"])
 
+
+def convert_to_mono(song):
+    mono_song = librosa.to_mono(song.T)
+    return mono_song
+
+
+def resample_audio(song, original_sr, target_sr):
+    resampled_song = librosa.resample(song, orig_sr=original_sr, target_sr=target_sr)
+    return resampled_song
+
+
 if uploaded_file is not None:
-    # Reading the uploaded file
-    data, samplerate = sf.read(uploaded_file)
+    song, samplerate = sf.read(uploaded_file)
     st.audio(uploaded_file, format='audio/wav')
 
-    # Display some audio info
-    duration = data.shape[0] / samplerate
+    duration = song.shape[0] / samplerate
     st.write(f"Sample Rate: {samplerate} Hz")
     st.write(f"Duration: {duration:.2f} seconds")
 
-    # get the samples of duration
-    time = np.linspace(0., duration, data.shape[0])
+    mono_song = convert_to_mono(song)
+    target_sr = 8000
+    resampled_song = resample_audio(mono_song, samplerate, target_sr)
+    new_duration = resampled_song.shape[0] / target_sr
+    time = np.linspace(0., new_duration, resampled_song.shape[0])
 
-    # Plot waveform using matplotlib and librosa
-    fig, ax = plt.subplots()
-    ax.plot(time, data, color='tab:blue')
+    fig, ax = plt.subplots(figsize=(10, 2.5))
+    ax.plot(time, resampled_song, color='tab:blue')
     ax.set(xlabel='Time (s)', ylabel='Amplitude')
-    ax.set_title("Waveform of the Audio File")
+    ax.set_title("Cleaned Waveform of the Audio File")
     ax.grid()
-
-    # Display plot in Streamlit
     st.pyplot(fig)
